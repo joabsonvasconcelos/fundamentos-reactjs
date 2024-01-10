5b7adf7a977cf2d1caa5d9db04cd219428288a29
Joabson Vasconcelos <119760277+joabsonvasconcelos@users.noreply.github.com>
2024-01-10 16:41:04 -0300

    update

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,13 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content}) {
+export function Comment({ content, onDeleteComment}) {
+function handleDeleteComment() {
+    console.log('deletar')
+
+    onDeleteComment(content);
+}
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
@@ -15,7 +21,7 @@ export function Comment({ content}) {
                         <time title='28 de dezembro de 2023 às 23:04h' dateTime='2023-05-11 08:13:30'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
                         </button>
                     </header>
