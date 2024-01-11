8837147b1f980398883c6f91a1ab6f134b78b943
Joabson Vasconcelos <119760277+joabsonvasconcelos@users.noreply.github.com>
2024-01-11 16:53:49 -0300

    Update Comment.jsx

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
 function handleDeleteComment() {
     console.log('deletar')
 
     onDeleteComment(content);
+}
+
+function handleLikeComment() {
+    setLikeCount((state) => {
+        return state + 1
+    });
 }
 
     return (
@@ -30,9 +39,9 @@ function handleDeleteComment() {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp size={20}/>
-                    Aplaudir <span>20</span>
+                    Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
